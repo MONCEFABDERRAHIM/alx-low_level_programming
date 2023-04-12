f1f1896576b5422be815ce0c08150f0e543509d4
Moncef <moncef.abderrahim7@gmail.com>
2023-04-11 17:39:39 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,17 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "main.h"
+
 /**
- * _strdup - prints the duplicate of a string
- * @str: parameter for duplicate string
- * Return: Success
+ *	str_concat - concatenate two strings
+ *	@s1: string one
+ *	@s2: string two
+ *	Return: pointer to concatenated string
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *s;
+	char *concatenatedString;
+	int s1Len, s2Len, i, j, bothLen;
 
-	s = malloc(strlen(str) + 1);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-	if (s == NULL)
+	s1Len = strlen(s1);
+	s2Len = strlen(s2);
+
+	concatenatedString = malloc(s1Len + s2Len + 1);
+	if (concatenatedString == NULL)
+	{
 		return (NULL);
-	strcpy(s, str);
-	return (s);
+	}
+
+	for (i = 0; i < s1Len; i++)
+	{
+		concatenatedString[i] = s1[i];
+	}
+	for (i = 0, j = s1Len, bothLen = s1Len + s2Len; j < bothLen; i++, j++)
+	{
+		concatenatedString[j] = s2[i];
+	}
+	concatenatedString[s1Len + s2Len] = '\0';
+
+	return (concatenatedString);
 }
